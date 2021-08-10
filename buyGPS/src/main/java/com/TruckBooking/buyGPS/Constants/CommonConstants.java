1bf936d4c41a8b5a06483ffd86afb9850b940722
Raksha <raksharagavendran@gmail.com>
2021-08-10 12:31:34 +0530

    trying to correct ppty file

--- buyGPS/src/main/java/com/TruckBooking/buyGPS/Constants/CommonConstants.java
+++ buyGPS/src/main/java/com/TruckBooking/buyGPS/Constants/CommonConstants.java
@@ -2,8 +2,7 @@ package com.TruckBooking.buyGPS.Constants;
 
 public class CommonConstants 
 {
- 
-	public static String AccountDoesntExist = "Account does not exist";
+    public static String AccountDoesntExist = "Account does not exist";
 	public static String deleteSuccess = "Account deleted successfully";
 	public static String updateSuccess ="Details updated successfully";
 }
